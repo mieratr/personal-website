3fea3123b83095373a8cf3945c5dc6b5752dbd66
mieratr <78476770+mieratr@users.noreply.github.com>
2023-01-23 09:00:09 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
+
+const text = "Hi, I'm Miera!";
+let i = 0;
+
+const typeWriter = () => {
+  if (i < text.length) {
+    document.getElementById("name").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 120);
+  }
+}
+typeWriter();
+
